307346f9aa862dad6d4237c84828b5f029d20f62
Minh Quang <quangminhnguyen3002@gmail.com>
2022-07-21 09:12:35 +0700

    buggg

---
+++ src/main/java/gmo/demowebsocketjava/DemoWebSocketJavaApplication.java
@@ -0,0 +1,13 @@
+package gmo.demowebsocketjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebSocketJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebSocketJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gmo/demowebsocketjava/DemoWebSocketJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package gmo.demowebsocketjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebSocketJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
